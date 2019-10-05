5c718df467e5f82487756ad3be78d047942df004
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2019-10-05 20:51:16 +0300

    Created global executable file

---
+++ global.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Stanislav Hrytcyshyn'
+
+'''
+Execute module
+'''
+
+import task178
+import task554
+
+def main():
+    '''Execute all programs'''
+
+    while True:
+        user_choice = input('Choose task to execute: (178 / 554): ')
+
+        if user_choice == '178':
+            task178.main()
+        elif user_choice == '554':
+            task554.main()
+        elif user_choice.lower() in ['q', 'quit', 'exit', 'stop', 'terminate']:
+            break
+        else:
+            print('Please choose task from (178 / 554)')
+            continue
+
+
+if __name__ == '__main__':
+    main()
