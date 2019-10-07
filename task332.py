a7fc3424eca7e83c540d7810d6f2c1e2461098cf
Oleksandr Krynytskyi <alex_rain1@hotmail.com>
2019-10-07 10:47:30 +0300

    Added task 332 and 88vg

---
+++ task332.py
@@ -0,0 +1,27 @@
+"""
+Task 02.
+Задание 332.
+Известно, что любое натуральное число можно представить
+в виде суммы не более чем четырех квадратов натуральных чисел или,
+что то же самое, в виде суммы четырех квадратов неотрицательных
+целых чисел (теорема Лагранжа). Дано натуральное n; указать такие
+неотрицательные x, y, z, t , что n = x^2 + y^2 + z^2 + t^2 .
+"""
+
+
+def lagrange(digit):
+    """Lagrange's four-square theorem"""
+    for _ in range(4):
+        _ = int(digit ** 0.5)
+        digit = digit - _ ** 2
+        print(_)
+
+
+def main():
+    """Main function to execute this module"""
+    print(__doc__)
+    try:
+        user_input = int(input("Enter your digit: "))
+        lagrange(user_input)
+    except ValueError:
+        print("Wrong input!")
